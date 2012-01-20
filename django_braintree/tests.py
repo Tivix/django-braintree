9862a46c260e1f9d40306254823eb3a809a59122
Dariusz Fryta <dariuszfryta@gmail.com>
2012-01-20 15:52:41 +0100

    Added fudged charge account test

--- django_braintree/tests.py
+++ django_braintree/tests.py
@@ -1,16 +1,40 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+import fudge
 from django.test import TestCase
+from django.contrib.auth.models import User
+from models import UserVault, PaymentLog
+from decimal import Decimal
+import braintree
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class FakeTransaction(object):
+    def __init__(self):
+        self.id = 1
+class FakeResponse(object):    
+    def __init__(self):
+        self.is_success = True
+        self.transaction = FakeTransaction()
+
+@fudge.patch('braintree.Transaction.sale')
+def fake_charge(vault, amount, FakeTransactionSale):
+    (FakeTransactionSale.expects_call()
+              .with_args()
+              .returns(FakeResponse())
+              )
+    vault.charge(Decimal(amount))
+              
+class PayTest(TestCase):
+    
+    def test_charge(self):              
+        
+        # Create user vault data
+        user = User.objects.create_user('test', 'test@tivix.com', 'test')
+        vault = UserVault.objects.create(user=user, vault_id="1cf373103e6657b96421348a")
+        
+        # Try charge account using FUDGE
+        fake_charge(vault, 10)
+        
+        # Check if PaymentLog is Saved
+        self.failUnlessEqual(PaymentLog.objects.all().count(), 1)
+        
+        
+    
