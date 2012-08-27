ac343036511f9f0c365a72d4a47d58c1495786d7
Sumit Chachra <sumit@tivix.com>
2012-08-27 15:45:35 -0700

    Version bump to 0.1.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ f.close()
 
 setup(
     name='tivix-django-braintree',
-    version='0.1',
+    version='0.1.1',
     author='Sumit Chachra',
     author_email='chachra@tivix.com',
     url='http://github.com/tivix/django-braintree',
